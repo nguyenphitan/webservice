060b11ba494589cbfe0cd82bf62bb89518872ab7
nguyenphitan <phitan2001@gmail.com>
2022-05-18 17:00:18 +0700

    update

--- Taco/src/main/java/com/nguyenphitan/taco/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nguyenphitan.taco.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nguyenphitan.taco.entity.Ingredient;
-
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-
-}
